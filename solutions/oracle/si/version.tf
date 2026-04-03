6243670a09003a13da99c96e50ee6c112cb86f43
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-04-03 22:46:12 +0000

    fix(deps): update terraform ibm to v2

--- solutions/oracle/si/version.tf
+++ solutions/oracle/si/version.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.89.0"
+      version = "2.0.0"
     }
     null = {
       source  = "hashicorp/null"
