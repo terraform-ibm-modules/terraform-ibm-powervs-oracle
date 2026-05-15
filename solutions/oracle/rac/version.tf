b8a0916dd6824180709a7e3c264ddca58a5959df
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-05-15 22:33:54 +0000

    fix(deps): update dependencies

--- solutions/oracle/rac/version.tf
+++ solutions/oracle/rac/version.tf
@@ -7,11 +7,11 @@ terraform {
     }
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.13.1"
+      version = ">= 0.14.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.2"
+      version = "~> 3.3"
     }
   }
 }
